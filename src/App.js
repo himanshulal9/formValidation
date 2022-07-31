aff3f815733c7970d04a5e139322ee5400515d11
himanshulal9 <himanshulal9@gmail.com>
2022-07-31 02:50:54 -0700

    getting started

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Getting starting with the form validation </h1>
+    </div>
+  );
+}
+
+export default App;
